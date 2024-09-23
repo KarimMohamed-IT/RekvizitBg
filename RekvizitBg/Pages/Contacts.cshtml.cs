6b0cf26e5ddac211e69d72820214a66a6c333a76
Admin <Admin@DESKTOP-RA4MML3>
2024-09-23 13:08:46 +0300

    Add project files.

---
+++ RekvizitBg/Pages/Contacts.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RekvizitBg.Pages
+{
+    public class ContactsModel : PageModel
+    {
+        private readonly ILogger<ContactsModel> _logger;
+
+        [BindProperty]
+        public string Name { get; set; }
+        [BindProperty]
+        public string Email { get; set; }
+        [BindProperty]
+        public string Message { get; set; }
+
+        public ContactsModel(ILogger<ContactsModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            ViewData["ActivePage"] = "Contacts"; // Set the active page here
+        }
+
+        public IActionResult OnPostSendMessage()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            // Логика за обработка на съобщението, като например изпращане на имейл или записване в база данни
+
+            TempData["SuccessMessage"] = "Вашето съобщение беше изпратено успешно!";
+            return RedirectToPage("/Contact");
+        }
+    }
+}
